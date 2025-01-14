b5bc80ab714ce2bf11f3339c55c0989713ad47d2
Ollie1994 <oliwer.carlsson.jobb@gmail.com>
2025-01-14 14:12:57 +0100

    added put and get, lets test

--- src/main/java/com/oliwer/plantswap/controllers/UserController.java
+++ src/main/java/com/oliwer/plantswap/controllers/UserController.java
@@ -1,4 +1,38 @@
 package com.oliwer.plantswap.controllers;
 
+import com.oliwer.plantswap.models.User;
+import com.oliwer.plantswap.repositories.PlantRepository;
+import com.oliwer.plantswap.repositories.UserRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
 public class UserController {
+
+    private final UserRepository userRepository;
+    private final PlantRepository plantRepository;
+
+    public UserController(UserRepository userRepository, PlantRepository plantRepository) {
+        this.userRepository = userRepository;
+        this.plantRepository = plantRepository;
+    }
+
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        User savedUser = userRepository.save(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedUser);
+
+    }
+
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers() {
+        List<User> users = userRepository.findAll();
+        return ResponseEntity.ok(users);
+    }
 }
