94a879f7c5b0ca38aadc8a4d79723450df91c7d6
Ollie1994 <oliwer.carlsson.jobb@gmail.com>
2025-01-14 11:40:25 +0100

    first commit

---
+++ src/main/java/com/oliwer/plantswap/PlantswapApplication.java
@@ -0,0 +1,13 @@
+package com.oliwer.plantswap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantswapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlantswapApplication.class, args);
+	}
+
+}
