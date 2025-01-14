49e20fe2e7b20be246357189df325cda52ff16c9
Ollie1994 <oliwer.carlsson.jobb@gmail.com>
2025-01-14 12:04:33 +0100

    fixat början på USER, ska fixa PLANT next

--- src/main/java/com/oliwer/plantswap/models/User.java
+++ src/main/java/com/oliwer/plantswap/models/User.java
@@ -1,4 +1,123 @@
 package com.oliwer.plantswap.models;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Document(collection = "users")
 public class User {
+
+    @Id
+    private String id;
+
+    private String username;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String profilePicture;
+
+    @DBRef
+    private List<String> plants;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public User() {
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+    //------------------------- Getters & Setters ------------------------------------------------------------------------
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getProfilePicture() {
+        return profilePicture;
+    }
+
+    public void setProfilePicture(String profilePicture) {
+        this.profilePicture = profilePicture;
+    }
+
+    public List<String> getPlants() {
+        return plants;
+    }
+
+    public void setPlants(List<String> plants) {
+        this.plants = plants;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
 }
